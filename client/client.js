1cf16b3cd9c58c5719337b3cd5cd9bdea7291120
Peyton Hammersley <voxelbee@gmail.com>
2020-01-27 01:10:14 +0000

    Fixed chunk loading and multiple clients working correctly

--- client/client.js
+++ client/client.js
@@ -1,75 +1,40 @@
 // SETTINGS
 const TILES_PER_CHUNK = 16;
-// SEND SETTINGS
+const VIEW_DISTANCE = 4;
+const FPS = 60;
+const TILE_SIZE = 32;
+const DEBUG = false;
+// END SETTINGS
 
 // Current size of the window
-var width;
-var height;
-
-// Is the main page enabled
-var mainPageOn = true;
+var width = window.innerWidth;
+var height = window.innerHeight;
 
 // Create the socket
 var socket = io();
 
-// This players unique id
-var uuid;
+// setup renderer and ticker
+var renderer = new PIXI.Renderer({ width: width, height: height, backgroundColor: 0x000000 });
+document.body.appendChild(renderer.view);
 
-// Size of ascii tiles
-const tileSize = 32;
+// Create a container for the chunk and add it to the stage
+const camera = new PIXI.Container();
 
-// Create the pixi application
-const app = new PIXI.Application({
-  width: window.innerWidth,
-  height: window.innerHeight,
-  backgroundColor: 0x000000,
-  resolution: window.devicePixelRatio || 1
-});
-document.body.appendChild(app.view);
+// Create different z layers
+var underLayer = new PIXI.Container();
+camera.addChild(underLayer);
 
+var overLayer = new PIXI.Container();
+camera.addChild(overLayer);
+
+// The reference to the tile set being used
 var tileSet;
 
 // Load in the tile set
 PIXI.Loader.shared.add("client/assets/charsets/dos40/data.json").load(setup);
 
-// If the window has been resized
-window.onresize = function(event) {
-  width = window.innerWidth;
-  height = window.innerHeight;
-  // Resize the app
-  app.resize(width, height);
-};
-
-// When a chunk is sent to the client
-socket.on("chunk-data", function(data) {
-  console.log("Recived chunk: " + data.position.x + "," +  + data.position.y);
-
-  // Create a container for the chunk and add it to the stage
-  const chunk = new PIXI.Container();
-  app.stage.addChild(chunk);
-
-  // Put the chunk contain in the right location
-  chunk.x = data.position.x * TILES_PER_CHUNK * tileSize;
-  chunk.y = data.position.y * TILES_PER_CHUNK * tileSize;
-
-  for(var i = 0; i < TILES_PER_CHUNK; i++) {
-    for(var j = 0; j < TILES_PER_CHUNK; j++) {
-      if(data.chunk.tiles[i * TILES_PER_CHUNK + j] != 0) {
-        //Create the tile sprite from the texture
-        var tile = new PIXI.Sprite(tileSet.textures["dos40-" + data.chunk.tiles[i * TILES_PER_CHUNK + j] + ".png"]);
-
-        //Position the tile sprite in the chunk
-        tile.x = i * tileSize;
-        tile.y = j * tileSize;
-        tile.height = tileSize;
-        tile.width = tileSize;
-
-        //Add the tile to the stage
-        chunk.addChild(tile);
-      }
-    }
-  }
-});
+var chunks = {};
+var entities = {};
 
 function setup() {
   tileSet = PIXI.Loader.shared.resources["client/assets/charsets/dos40/data.json"].spritesheet;
@@ -80,3 +45,159 @@ function setup() {
   // Send a loaded message to the sever
   socket.emit("loaded");
 }
+
+// setup RAF
+var oldTime = Date.now();
+
+requestAnimationFrame(animate);
+function animate() {
+    var newTime = Date.now();
+    var deltaTime = newTime - oldTime;
+    oldTime = newTime;	
+    if (deltaTime < 0) deltaTime = 0;
+    if (deltaTime > 1000) deltaTime = 1000;
+    var deltaFrame = deltaTime * 60 / 1000; //1.0 is for single frame
+	
+    renderer.render(camera);
+
+    requestAnimationFrame(animate);
+}
+
+// If the window has been resized
+window.onresize = function(event) {
+  width = window.innerWidth;
+  height = window.innerHeight;
+
+  // Resize the app
+  renderer.resize(width, height);
+};
+
+// When a chunk is sent to the client
+socket.on("chunk-data", function(data) {
+  for(var i = 0; i < TILES_PER_CHUNK; i++) {
+    for(var j = 0; j < TILES_PER_CHUNK; j++) {
+      setTile(i + (data.position.x * TILES_PER_CHUNK), j + (data.position.y * TILES_PER_CHUNK), data.chunk.tiles[i * TILES_PER_CHUNK + j]);
+    }
+  }
+});
+
+socket.on("set-tile", function(data) {
+  setTile(data.x, data.x, data.tile);
+});
+
+socket.on("add-entity", function(data) {
+  //Create the tile sprite from the texture
+  var tile = new PIXI.Sprite(tileSet.textures["dos40-" + data.charId + ".png"]);
+
+  //Position the tile sprite in the chunk
+  tile.x = data.x * TILE_SIZE;
+  tile.y = data.y * TILE_SIZE;
+  tile.height = TILE_SIZE;
+  tile.width = TILE_SIZE;
+
+  // Set the color of the tile
+  tile.tint = data.colorId;
+
+  entities[data.uuid] = tile;
+  overLayer.addChild(entities[data.uuid]);
+});
+
+socket.on("update-entity", function(data) {
+  entities[data.uuid].x = data.x * TILE_SIZE;
+  entities[data.uuid].y = data.y * TILE_SIZE;
+});
+
+socket.on("remove-entity", function(data) {
+  overLayer.removeChild(entities[data.uuid]);
+});
+
+socket.on("remove-chunk", function(data) {
+  underLayer.removeChild(chunks[data.x + "," + data.y].container);
+  delete chunks[data.x + "," + data.y];
+});
+
+// Sets the tile at location specified with object tileData {charId: , colorId: }
+function setTile(x, y, tileData) {
+  var chunkX = Math.floor(x / TILES_PER_CHUNK);
+  var chunkY = Math.floor(y / TILES_PER_CHUNK);
+  x = x % TILES_PER_CHUNK;
+  y = y % TILES_PER_CHUNK;
+  if(x < 0) x += TILES_PER_CHUNK;
+  if(y < 0) y += TILES_PER_CHUNK;
+
+  //Create the tile sprite from the texture
+  var tile = new PIXI.Sprite(tileSet.textures["dos40-" + tileData.charId + ".png"]);
+
+  //Position the tile sprite in the chunk
+  tile.x = x * TILE_SIZE;
+  tile.y = y * TILE_SIZE;
+  tile.height = TILE_SIZE;
+  tile.width = TILE_SIZE;
+
+  // Set the color of the tile
+  tile.tint = tileData.colorId;
+
+  // If the chunk exists add the tile to the chunk and remove old tile
+  if(chunks.hasOwnProperty(chunkX + "," + chunkY)) {
+    // If there was a previous tile at this location remove it
+    if(chunks[chunkX + "," + chunkY].tiles.hasOwnProperty(x + "," + y)) {
+      chunks[chunkX + "," + chunkY].container.removeChild(chunks[chunkX + "," + chunkY].tiles[x + "," + y]);
+    }
+  } else {
+    // Creates a new chunk data object
+    chunks[chunkX + "," + chunkY] = {
+      container: new PIXI.Container(),
+      tiles: {}
+    };
+    underLayer.addChild(chunks[chunkX + "," + chunkY].container);
+
+    // Put the chunk contain in the right location
+    chunks[chunkX + "," + chunkY].container.x = chunkX * TILES_PER_CHUNK * TILE_SIZE;
+    chunks[chunkX + "," + chunkY].container.y = chunkY * TILES_PER_CHUNK * TILE_SIZE;
+
+    // Add chunk borders to be visible
+    if(DEBUG) {
+      var graphics = new PIXI.Graphics();
+      graphics.lineStyle(5, 0xFF0000);
+      graphics.drawRect(0, 0, TILES_PER_CHUNK * TILE_SIZE, TILES_PER_CHUNK * TILE_SIZE);
+      chunks[chunkX + "," + chunkY].container.addChild(graphics);
+    }
+  }
+
+  if(tileData.charId != 0) {
+    //Add the tile to the stage
+    chunks[chunkX + "," + chunkY].container.addChild(tile);
+    chunks[chunkX + "," + chunkY].tiles[x + "," + y] = tile;
+  }
+}
+
+socket.on("player-position", function(data) {
+  camera.x = (width / 2) - (data.x * TILE_SIZE);
+  camera.y = (height / 2) - (data.y * TILE_SIZE);
+})
+
+
+document.addEventListener('keydown', function(event) {
+  if(event.code == "KeyW") {
+    socket.emit("move-key", {key: "up", press: true});
+    console.print("Hello");
+  } else if(event.code == "KeyD") {
+    socket.emit("move-key", {key: "right", press: true});
+  } else if(event.code == "KeyS") {
+    socket.emit("move-key", {key: "down", press: true});
+  } else if(event.code == "KeyA") {
+    socket.emit("move-key", {key: "left", press: true});
+  }
+});
+
+document.addEventListener('keyup', function(event) {
+  if(event.code == "KeyW") {
+    socket.emit("move-key", {key: "up", press: false});
+  } else if(event.code == "KeyD") {
+    socket.emit("move-key", {key: "right", press: false});
+  } else if(event.code == "KeyS") {
+    socket.emit("move-key", {key: "down", press: false});
+  } else if(event.code == "KeyA") {
+    socket.emit("move-key", {key: "left", press: false});
+  }
+});
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // SETTINGS
 const VIEW_DISTANCE = 4;
 const TILES_PER_CHUNK = 16;
-// SEND SETTINGS
+// END SETTINGS
 
 console.log("Server starting...");
 var express = require("express");
@@ -25,7 +25,7 @@ app.use("/client", express.static(__dirname + "/client"));
 const fastnoise = require('fastnoisejs');
 const noise = fastnoise.Create(10)
  
-noise.SetNoiseType(fastnoise.Simplex)
+noise.SetNoiseType(fastnoise.Cellular)
 
 // All the players on the server
 var players = {};
@@ -40,19 +40,68 @@ io.sockets.on("connection", function(socket)
   var uuid = makeUUID();
 
   // Add the player to the players map
-  players[uuid] = {position: {x: 0, y: 0}, socket: socket, loaded: false};
+  players[uuid] = {
+    position: {x: 0, y: 0},
+    color: Math.random() * 0xFFFFFF,
+    socket: socket,
+    loaded: false,
+    sentChunks: {},
+    velocity: {x: 0,  y: 0},
+    acceleration: {x: 0, y: 0},};
   console.log("Conected: " + uuid);
+
+  // Add the joined player to everyones view except this player
+  socket.broadcast.emit("add-entity", {
+    x: players[uuid].position.x,
+    y: players[uuid].position.y,
+    charId: 1,
+    colorId: players[uuid].color,
+    uuid: uuid
+  });
 
   // When the player disconects
   socket.on("disconnect", function()
   {
     delete players[uuid];
     console.log("Disconected: " + uuid);
+    socket.broadcast.emit("remove-entity", {uuid: uuid});
   });
 
   socket.on("loaded", function() {
     players[uuid].loaded = true;
-  })
+    socket.emit("player-position", { x: players[uuid].position.x, y: players[uuid].position.y });
+    // Get all the players
+    const playerKeys = Object.keys(players);
+    for(var uuidL of playerKeys) {
+      // Add the player to the client
+      socket.emit("add-entity", {
+        x: players[uuidL].position.x,
+        y: players[uuidL].position.y,
+        charId: 1,
+        colorId: players[uuidL].color,
+        uuid: uuidL
+      });
+    }
+  });
+
+  socket.on("move-key", function(data) {
+    if(data.key == "up") {
+      players[uuid].position.y--;
+    } else if(data.key == "down") {
+      players[uuid].position.y++;
+    } else if(data.key == "left") {
+      players[uuid].position.x--;
+    } else if(data.key == "right") {
+      players[uuid].position.x++;
+    }
+
+    io.emit("update-entity", {
+      x: players[uuid].position.x,
+      y: players[uuid].position.y,
+      uuid: uuid
+    })
+    socket.emit("player-position", {x: players[uuid].position.x, y: players[uuid].position.y});
+  });
 });
 
 // Creates a unique id
@@ -84,13 +133,14 @@ setInterval(function()
     var playerChunkX = Math.floor(players[uuid].position.x / TILES_PER_CHUNK);
     var playerChunkY = Math.floor(players[uuid].position.y / TILES_PER_CHUNK);
 
+    // Send all chunks that are needed to the player
     for(var i = -VIEW_DISTANCE + 1; i < VIEW_DISTANCE; i++) {
       for(var j = -VIEW_DISTANCE + 1; j < VIEW_DISTANCE; j++) {
         var currentChunkX = playerChunkX + i;
         var currentChunkY = playerChunkY + j;
 
         // Checks if the current chunk the player is in has been loaded
-        if(!(chunks.hasOwnProperty(currentChunkX + "," + currentChunkY))) {
+        if(!chunks.hasOwnProperty(currentChunkX + "," + currentChunkY)) {
           // Generate the chunk and add it to the loaded chunks on the server
           chunks[(currentChunkX + "," + currentChunkY)] = generateChunk(currentChunkX, currentChunkY);
 
@@ -99,12 +149,37 @@ setInterval(function()
             position: {x: currentChunkX, y: currentChunkY},
             chunk: chunks[(currentChunkX + "," + currentChunkY)]
           });
+
+          // Set the chunk as sent
+          players[uuid].sentChunks[currentChunkX + "," + currentChunkY] = true;
+        } else {
+          // If the player hasn't recived this chunk
+          if(!players[uuid].sentChunks.hasOwnProperty(currentChunkX + "," + currentChunkY)) {
+            // Send the chunk to the player
+            players[uuid].socket.emit("chunk-data", {
+              position: {x: currentChunkX, y: currentChunkY},
+              chunk: chunks[(currentChunkX + "," + currentChunkY)]
+            });
+            // Set the chunk as sent
+            players[uuid].sentChunks[currentChunkX + "," + currentChunkY] = true;
+          }
+        }
+      }
+
+      // Remove chunks that are not needed by the clients
+      const sentChunkKeys = Object.keys(players[uuid].sentChunks)
+      for(var sentChunk of sentChunkKeys) {
+        var position = sentChunk.split(",");
+        if(Math.sqrt(Math.pow(Math.abs(position[0] - playerChunkX), 2) + Math.pow(Math.abs(position[1] - playerChunkY), 2)) > VIEW_DISTANCE) {
+          delete players[uuid].sentChunks[sentChunk];
+          players[uuid].socket.emit("remove-chunk", {x: position[0], y: position[1]});
         }
       }
     }
   }
-}, 1000 / 120)
+}, 1000 / 25)
 
+// Generates terrain and tiles for a chunk
 function generateChunk(x, y) {
   console.log("Generating chunk: " + x + "," + y);
 
@@ -112,10 +187,36 @@ function generateChunk(x, y) {
 
   for(var i = 0; i < TILES_PER_CHUNK; i++) {
     for(var j = 0; j < TILES_PER_CHUNK; j++) {
-      var noiseValue = Math.floor((noise.GetNoise((x * TILES_PER_CHUNK) + i, (y * TILES_PER_CHUNK) + j) + 1) * 128);
+      var noiseValue = generateTerrainTile((x * TILES_PER_CHUNK) + i, (y * TILES_PER_CHUNK) + j);
       tiles.push(noiseValue);
     }
   }
 
   return {tiles: tiles};
+}
+
+// Returns a tile object {charId: , colorId: } for the specified location
+function generateTerrainTile(x, y) {
+  var noiseValue = Math.floor((noise.GetNoise(x * 8, y * 8) + 1) * 128);
+  return {charId: noiseValue, colorId: noiseValue};
+}
+
+// Sets a tile in a specified location tile data = {charId: , colorId: }
+function setTile(x, y, tileData) {
+  // Send the tile to all the clients
+  io.emit("set-tile", {
+    x: x,
+    y: y,
+    tile: tileData
+  });
+
+  var chunkX = Math.floor(x / TILES_PER_CHUNK);
+  var chunkY = Math.floor(y / TILES_PER_CHUNK);
+  x = x % TILES_PER_CHUNK;
+  y = y % TILES_PER_CHUNK;
+  if(x < 0) x += TILES_PER_CHUNK;
+  if(y < 0) y += TILES_PER_CHUNK;
+
+  // Set the chunk to contain that tile
+  chunks[chunkX + "," + chunkY].tiles[x * TILES_PER_CHUNK + y] = tileData;
 }
